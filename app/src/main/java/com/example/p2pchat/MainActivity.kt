f530ebce7d63d09b4d96b571fcf2c2eca1f9a03b
Kevin Zhang <keevee810@gmail.com>
2021-04-11 12:47:28 -0400

    flipped the chat

--- app/src/main/java/com/example/p2pchat/MainActivity.kt
+++ app/src/main/java/com/example/p2pchat/MainActivity.kt
@@ -167,6 +167,9 @@ class MainActivity : AppCompatActivity() {
             userWithToken.deviceToken = token
 
             loginUser(currentUser)
+        }.addOnFailureListener {exception ->
+            Log.e("Firebase token", "Token not properly retrieved")
+            exception.printStackTrace()
         }
     }
 
